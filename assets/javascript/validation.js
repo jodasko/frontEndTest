1e5a895ad991248916be31eeb444ca37372029a8
Carlos Salazar <carlos.salazar@key4events.com>
2021-03-18 17:35:03 +0100

    bg blue, validation messages

--- assets/javascript/validation.js
+++ assets/javascript/validation.js
@@ -5,29 +5,32 @@ const modeloAuto = document.querySelector('#modelo_del_auto');
 const anoAuto = document.querySelector('#ano_auto');
 const email = document.querySelector('#email');
 
-alert_msg.classList.add('invisible');
+alert_msg.classList.add('d-none');
+
+
+/*hola*/
 
 submit_btn.onclick = (event) => {
   event.preventDefault();
 
   if (marcaAuto.value == 'null' || marcaAuto.value == '') {
-    alert_msg.classList.remove('invisible');
+    alert_msg.classList.remove('d-none');
     return (document.querySelector('.message__validate').innerHTML =
       'Por favor elija una Marca');
   }
 
   if (modeloAuto.value === 'null' || modeloAuto.value === '') {
-    alert_msg.classList.remove('invisible');
+    alert_msg.classList.remove('d-none');
     return (document.querySelector('.message__validate').innerHTML =
       'Por favor elija el un Modelo');
   }
 
   if (anoAuto.value === 'null' || anoAuto.value === '') {
-    alert_msg.classList.remove('invisible');
+    alert_msg.classList.remove('d-none');
     return (document.querySelector('.message__validate').innerHTML =
       'Por favor elija el Anio');
   }
 
-  alert_msg.classList.add('invisible');
+  alert_msg.classList.add('d-none');
   return (document.querySelector('.message__validate').innerHTML = ' ');
 };
